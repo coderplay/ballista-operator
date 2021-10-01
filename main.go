4d927ca981f91d7c775a95210352699790995732
Min Zhou <min.zhou@databricks.com>
2021-09-30 20:47:17 -0700

    kubebuilder create webhook --version v1 --kind BallistaApplication --defaulting --programmatic-validation

--- main.go
+++ main.go
@@ -85,6 +85,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "BallistaApplication")
 		os.Exit(1)
 	}
+	if err = (&ballistaminzhouinfov1.BallistaApplication{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "BallistaApplication")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
