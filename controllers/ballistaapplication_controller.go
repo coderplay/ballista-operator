d088a166c91230941490c342b2370e038e92c92a
Min Zhou <min.zhou@databricks.com>
2021-10-01 15:09:21 -0700

    start reconclier logic

--- controllers/ballistaapplication_controller.go
+++ controllers/ballistaapplication_controller.go
@@ -39,17 +39,13 @@ type BallistaApplicationReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the BallistaApplication object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.8.3/pkg/reconcile
 func (r *BallistaApplicationReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// your logic here
+
 
 	return ctrl.Result{}, nil
 }
